e196c6b02f31b5ba6ddac8327d32a69e8cdb10e0
Shiva Kondapalli <1993.ksk@gmail.com>
2019-06-22 19:12:30 +0530

    get_data through comman line parser

--- get_data.py
+++ get_data.py
@@ -1,13 +1,34 @@
 import pandas as pd
+import argparse
+from os import path
 
-path = 'https://raw.githubusercontent.com/WillKoehrsen/Bokeh-Python-Visualization/master/bokeh_app/data/flights.csv'
-df = pd.read_csv(path, sep=',')
+write_directory = 'data/'
 
-print(df.head(15))
-print(df.shape)
 
-df = df.drop('Unnamed: 0', axis=1)
+def get_data(file_path):
+    """function that takes a file on the web and saves it locally"""
+    df = pd.read_csv(file_path, sep=',')
+    return df.to_csv(path.join(write_directory) + file_path.split('/')[-1])
 
-df.to_csv('data/flights.csv')
+
+if __name__ == '__main__':
+
+    # instantiate ArgumentParser class
+    parser = argparse.ArgumentParser()
+
+    # add argument to parser object
+    parser.add_argument("file_path", type=str, help='enter file you want to download')
+
+    # the argument passed into the parser is
+    args = parser.parse_args()
+
+    # pass this is argument to the file_path parameter
+    get_data(file_path=args.file_path)
+
+
+
+
+
+
 
 
